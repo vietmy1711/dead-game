a9ebf5a7bbe1b27474ca29a703db3065fdf577d7
Nguyen Viet My <zzvietmyzz@gmail.com>
2023-03-07 02:14:51 +0700

    Init base project

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import '../game/dead_game.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dead Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GameWidget<DeadGame>(game: DeadGame()),
+    );
+  }
+}
